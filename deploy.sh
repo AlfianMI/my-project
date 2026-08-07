169135fa5d51e803f1f141b7228b6b246b58563f
AlfianMI <sulivian321@gmail.com>
2026-08-07 14:08:16 +0700

    Add Deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Menentukan variabel
+LOG_DIR="/var/log"
+TARGET_HOST="google.com"
+
+echo "=== System Health Check ==="
+echo "Tanggal & Waktu: $(date)"
+
+# Checking Disk Usage
+echo -e "\n1. Pemakaian Disk Storage:"
+df -h /
+
+# Checking Connectivity
+echo -e "\n2. Pengujian Koneksi ke $TARGET_HOST:"
+if ping -c 2 $TARGET_HOST > /dev/null 2>&1; then
+    echo "[OK] Koneksi internet lancar."
+else
+    echo "[ERROR] Tidak dapat terhubung ke internet!"
+fi
